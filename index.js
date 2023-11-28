b7e8f289c72f338dd62c3a06b0830a02370b7597
Moritz Rose <moritz_rose@yahoo.de>
2023-11-28 10:10:51 +0100

    fixed Date

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 let container = document.querySelector(".container")
 let doors = document.querySelectorAll(".door")
-const currentDate = new Date();
+
+function getCurrentDate(){
+    return new Date()
+}
+
+let currentDate = getCurrentDate()
+let currentDay = currentDate.getDate()
 
 let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24]
 let events = ["🎄","⛄","🛷","🦌","❄️","🎁","🎅","🌟","🕯️","🔔","🍫","🤶🏼","🐕","🍪","🍬","🍦","🎂","🍨","🍭","🧸","🪁","🎷","💐","🕹️",]
@@ -8,7 +14,6 @@ numbers.sort((a, b) => 0.5 - Math.random());
 for (let door of doors) {
     let doorNumber = numbers.pop()
     door.innerText = doorNumber
-    let currentDay = 13
     door.addEventListener("click", () => {
         if(currentDate.getMonth()+2<12){
             alert("It is not December yet!!")
@@ -18,7 +23,7 @@ for (let door of doors) {
                 alert("You still have to wait " + (doorNumber - currentDay) + " days, to open that :)")
             }
             if (doorNumber <= currentDay){
-                door.innerText = events[doorNumber]
+                door.innerText = events[doorNumber-1]
             }
         }
     })
@@ -27,4 +32,5 @@ for (let door of doors) {
 document.body.addEventListener("click", () => {
     let audio = document.getElementById("music")
     audio.play()
+
 })
